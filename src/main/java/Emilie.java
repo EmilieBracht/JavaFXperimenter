bd477ed7b31ade08bcb9a9190b900552c95c41dc
EmilieBracht <emiliebracht@gmail.com>
2021-02-09 11:48:03 +0100

    Hej

---
+++ src/main/java/Emilie.java
@@ -0,0 +1,13 @@
+public class Emilie {
+
+    public double getHøjde() {
+        return højde;
+    }
+
+    public void setHøjde(double højde) {
+        this.højde = højde;
+    }
+
+    double højde = 1.65;
+
+}
---
+++ src/main/java/HelloFX.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        String version = System.getProperty("javafx.version");
+        Label label= new Label("Hello from JavaFX" + version);
+        StackPane stackPane = new StackPane(label);
+        Scene scene = new Scene(stackPane, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
